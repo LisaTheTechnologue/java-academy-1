5c5a997d8c8ad88a122c599baacb4adf01dcb15d
LisaTheTechnologue <jolisa.chan.store@gmail.com>
2024-07-01 17:44:42 +0700

    mock 1

--- src/main/java/com/example/SpringMVCDemo/GreetingController.java
+++ src/main/java/com/example/SpringMVCDemo/GreetingController.java
@@ -9,40 +9,20 @@ import java.util.List;
 
 @Controller
 public class GreetingController {
-
-    @GetMapping("/greetingDemo")
-    public String greetingDemo() {
-        return "greeting";
-    }
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                           Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
     @GetMapping("/greetings")
     public String greetings(Model model) {
-        List<Greeting> greetings1 = Greeting.readGreetings();
-        model.addAttribute("greetings", greetings1);
-        return "greeting_btvn2";
-    }
-
-    @GetMapping("/greeting-btvn3")
-    public String greetingBTVN3(Model model) {
         model.addAttribute("greetings", Greeting.readGreetings());
         model.addAttribute("greeting", new Greeting());
-        return "greeting_btvn3";
+        return "greeting";
     }
 
-    @PostMapping("/greeting-btvn3")
+    @PostMapping("/greetings")
     public String create(@ModelAttribute("greeting") Greeting greeting) {
         Greeting.createGreeting(greeting);
-        return "redirect:/greeting-btvn3";
+        return "redirect:/greetings";
     }
 
-    @GetMapping("/greeting-btvn3/update/{id}")
+    @GetMapping("/greetings/update/{id}")
     public String editForm(@PathVariable("id") int id,
                            Model model) {
         Greeting greeting1 = Greeting.getGreetingById(id);
@@ -50,28 +30,28 @@ public class GreetingController {
             model.addAttribute("greeting", greeting1);
         } else {
             model.addAttribute("error", "invalid id");
-            return "greeting_btvn3";
+            return "greeting";
         }
-        return "greeting_btvn3_edit";
+        return "greeting_edit";
     }
 
-    @PostMapping("/greeting-btvn3/update/{id}")
+    @PostMapping("/greetings/update/{id}")
     public String updateGreeting(@PathVariable("id") int id,
                                  @ModelAttribute("greeting") Greeting greeting,
                                  Model model) {
         if (!Greeting.updateGreeting(id,greeting)) {
             model.addAttribute("error", "invalid id");
-            return "redirect:/greeting-btvn3/update/{id}";
+            return "redirect:/greetings/update/{id}";
         }
-        return "redirect:/greeting-btvn3"; // Handle update failure (e.g., invalid ID)
+        return "redirect:/greetings"; // Handle update failure (e.g., invalid ID)
     }
 
-    @GetMapping("/greeting-btvn3/delete/{id}")
+    @GetMapping("/greetings/delete/{id}")
     public String deleteGreeting(@PathVariable("id") int id,
                                  Model model) {
         if (!Greeting.deleteGreeting(id)) {
             model.addAttribute("error", "invalid id");
         }
-        return "redirect:/greeting-btvn3"; // Handle deletion failure (e.g., invalid ID)
+        return "redirect:/greetings"; // Handle deletion failure (e.g., invalid ID)
     }
 }
